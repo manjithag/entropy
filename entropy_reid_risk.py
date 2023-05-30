2b27427b67d119d2036ed675422278c78cd64088
Manjitha <manjitha.ee@gmail.com>
2023-05-30 14:59:17 +0200

    Add calculation of H(R|s) and k_eff

--- entropy_reid_risk.py
+++ entropy_reid_risk.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def normal_dist(x, mean, sd):
 
-    prob_density = (np.pi*sd) * np.exp(-0.5*((x-mean)/sd)**2)
+    prob_density = 1/(np.sqrt(2 * np.pi) * sd) * np.exp(-0.5*((x-mean)/sd)**2)
 
     return prob_density
 
@@ -23,26 +23,33 @@ def calc_k_eff_s(original_df : pd.DataFrame, annonymized_df : pd.DataFrame):
         quasi_identifier = 'q1'
 
         original_series = original_df[quasi_identifier]
-        #annonymized_series = annonymized_df[quasi_identifier]
+        annonymized_series = annonymized_df[quasi_identifier]
 
         #print(original_series)
 
-        sd = 10
+        entropy_list =[]
+        k_eff_list = []
 
+        sd = 10
 
         for si in original_series:
 
             entropy_h_rs = 0
-            for ri in annonymized_df:
+            for ri in annonymized_series:
 
                 p_rs = normal_dist(ri,si,sd)
                 log_p_rs = np.log2(p_rs)
                 entropy_h_rs += p_rs * log_p_rs
 
-            print(entropy_h_rs)
+            entropy_h_rs = -1 * entropy_h_rs
+            k_eff_s = 2 ** entropy_h_rs
 
+            entropy_list.append(entropy_h_rs)
+            k_eff_list.append(k_eff_s)
 
+        output_df = pd.DataFrame({'H(R|s)' : entropy_list, 'k eff' : k_eff_list})
 
+    return output_df
 
 
 
