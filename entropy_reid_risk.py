72e81ff971a7271aae01d74ec12e55fb9011f479
Manjitha <manjitha.ee@gmail.com>
2023-05-30 18:21:02 +0200

    Add comments & update function names

--- entropy_reid_risk.py
+++ entropy_reid_risk.py
@@ -1,65 +1,70 @@
+###  This method of calculating risk of re-identification refers to the research paper of
+###  'M. Bezzi, “An Entropy based method for Measuring Anonymity'.
+
 import pandas as pd
 import numpy as np
-
 def normal_dist(x, mean, sd):
+    ## Calculating the probablity value according to normal distribution
+            ##  Arguments   : Variable (x), Mean, Standard Diviation (sd)
+            ##  Returns     : probability
 
     prob_density = 1/(np.sqrt(2 * np.pi) * sd) * np.exp(-0.5*((x-mean)/sd)**2)
 
     return prob_density
 
+def calc_parameters(original_df : pd.DataFrame, anonymized_df : pd.DataFrame, quasi_identifier : str ):
 
-def calc_k_eff_s(original_df : pd.DataFrame, annonymized_df : pd.DataFrame):
+    ## Calculate following parameters
+            # Entropy = H(R|s)
+            # k_eff (s)
+            # ECM
+            # NTM
+    ## Arguments : Original and Anonymized datasets, column name of quasi identifier
 
     no_records_original = original_df.shape[0]
-    no_records_annonymized = annonymized_df.shape[0]
+    no_records_anonymized = anonymized_df.shape[0]
 
-    #print(original_df)
-
-    if no_records_original != no_records_annonymized:
+    if no_records_original != no_records_anonymized:
         print('Error : No of records are different in original and annonymized datasets')
 
     else:
-
-        quasi_identifier = 'q1'
+       # quasi_identifier = 'q1'
 
         original_series = original_df[quasi_identifier]
-        annonymized_series = annonymized_df[quasi_identifier]
+        anonymized_series = anonymized_df[quasi_identifier]
 
-        #print(original_series)
-
-        entropy_list =[]
-        k_eff_list = []
+        entropy_list =[]        # A list to store entropy values of each record
+        k_eff_list = []         # A list to store k_eff values of each record
         ecm = 0
         ntm = 0
         sd = 10
 
 
-        for i in range(no_records_original):
+        for i in range(no_records_original):    # Looping through records in original data set
             si = original_series[i]
 
-            entropy_h_rs = 0
-            p_rs_list = []
-            for ri in annonymized_series:
+            entropy_h_rs = 0                    # Entropy = H(R|s)
+            p_rs_list = []                      # A list to store P(R|s)
+            for ri in anonymized_series:        # Looping through records in anonymized data set
 
-                p_rs = normal_dist(ri,si,sd)
+                p_rs = normal_dist(ri,si,sd)    # Probability for individual records
                 log_p_rs = np.log2(p_rs)
-
-                ##  Calculating NTM
-                p_rs_list.append(p_rs)
-
                 entropy_h_rs += p_rs * log_p_rs
 
-            entropy_h_rs = -1 * entropy_h_rs
-            k_eff_s = 2 ** entropy_h_rs
+                p_rs_list.append(p_rs)          #  Storing P(r|s) for a 's' value of each records (For NTM)
 
-            entropy_list.append(entropy_h_rs)
-            k_eff_list.append(k_eff_s)
+            entropy_h_rs = -1 * entropy_h_rs    # Calculating H(R|s) = SIGMA {P(R|s) x log2[P(R|s)]} for a 's' value
+            k_eff_s = 2 ** entropy_h_rs         # Calculating k_eff for a 's' value
 
-            ecm += 1 / k_eff_s
+            entropy_list.append(entropy_h_rs)   # Entropy list for all the 's' values
+            k_eff_list.append(k_eff_s)          # k_eff list for all the 's' values
 
-            max_prob_index = p_rs_list.index(max(p_rs_list))
+            ecm += 1 / k_eff_s                  # Calculating ECM for a 's' value
 
-            if max_prob_index == i:
+            ## Calculation of NTM
+            max_prob_index = p_rs_list.index(max(p_rs_list))        # Getting index of the maximum P(r|s) for a 's'
+
+            if max_prob_index == i:             # Verification if the maximum P(r|s) corresponds to an actual match
                 ntm += 1
                 print(i)
 
--- test_entropy_reid_risk.py
+++ test_entropy_reid_risk.py
@@ -1,16 +1,16 @@
 import pandas as pd
-from entropy_reid_risk import calc_k_eff_s
+from entropy_reid_risk import calc_parameters
 
 ## Test Case 1
 # q1 : Quasi Identifier
 original_df = pd.DataFrame({'No': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
                     'q1': [88, 21, 90, 69, 25, 30, 60, 42, 14, 28, 35, 44, 15, 55, 72, 18]})
 
-annonymized_df = pd.DataFrame({'No': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+anonymized_df = pd.DataFrame({'No': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
                     'q1': [90, 22, 88, 71, 24, 28, 58, 39, 16, 30, 42, 46, 14, 61, 71, 20]})
 
 
-calc_k_eff_s(original_df,annonymized_df)
+calc_parameters(original_df,anonymized_df,'q1')
 
 #print(original_df)
 #original_series = original_df['s1']
