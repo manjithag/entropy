fdd90e6910e105ee942954c2248e8278446a5701
Manjitha <manjitha.ee@gmail.com>
2023-06-01 16:16:16 +0200

    Improved structure

--- test_entropy_reid_risk.py
+++ test_entropy_reid_risk.py
@@ -18,5 +18,5 @@ anonymized_df2 = pd.DataFrame({'No': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
                     'q1': [88, 21, 90, 69, 25, 30, 60, 42, 14, 28, 35, 44, 15, 55, 72, 18]})
 
 
- calc_parameters(original_df1,anonymized_df1,'q1')
+calc_parameters(original_df1,anonymized_df1,'q1')
 #calc_parameters(original_df2,anonymized_df2,'q1')
